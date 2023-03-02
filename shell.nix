571e3927c13bb0d55f0fc0ff29cf5cc507a145e5
Zoe Roux <zoe.roux@zoriya.dev>
2023-03-02 15:31:31 +0900

    Adding required methods

--- shell.nix
+++ shell.nix
@@ -4,6 +4,7 @@ pkgs.mkShell {
 		cargo
 		rustfmt
 		rustc
+		libnotify
 	];
 
 	RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use std::{error::Error, future::pending};
-use zbus::{dbus_interface, ConnectionBuilder};
+use std::error::Error;
+use zbus::{Connection, zvariant::{DeserializeDict, SerializeDict, Type}};
+use zbus::dbus_interface;
 
 struct NotifManager {
 	next_id: u32,
+}
+
+#[derive(DeserializeDict, SerializeDict, Type)]
+#[zvariant(signature = "dict")]
+struct Hints {
+	category: Option<String>,
+	urgency: Option<u8>,
 }
 
 #[dbus_interface(name = "org.freedesktop.Notifications")]
@@ -14,25 +22,41 @@ impl NotifManager {
 		app_icon: &str,
 		summary: &str,
 		body: &str,
-		// actions: as,
-		// hints: a{sv},
+		actions: Vec<&str>,
+		hints: Hints,
 		expire_timeout: i32,
 	) -> u32 {
 		self.next_id += 1;
 		println!("{}: {}", summary, body);
 		self.next_id
 	}
+
+	fn get_capabilities(&self) -> Vec<&str> {
+		vec!["body", "actions", "body-images", "persistence", "icon-static"]
+	}
+
+	fn get_server_information(&self) -> [&str; 4] {
+		[
+			env!("CARGO_PKG_NAME"),
+			"zoriya",
+			env!("CARGO_PKG_VERSION"),
+			"1.2",
+		]
+	}
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+	let connection = Connection::session().await?;
 	let manager = NotifManager { next_id: 0 };
-	let _ = ConnectionBuilder::session()?
-		.name("org.freedesktop.Notifications")?
-		.serve_at("/org/freedesktop/Notifications", manager)?
-		.build()
+
+	connection
+		.object_server()
+		.at("/org/freedesktop/Notifications", manager)
+		.await?;
+	connection
+		.request_name("org.freedesktop.Notifications")
 		.await?;
 
-	pending::<()>().await;
-	Ok(())
+	loop {}
 }
